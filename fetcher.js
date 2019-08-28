83f8aa5965a082ac18047c71decd3b75e0483ae8
Gary Chen <garychen.whc@gmail.com>
2019-08-28 19:27:58 +0000

    small changes

--- fetcher.js
+++ fetcher.js
@@ -14,7 +14,7 @@ request(`${args[0]}`, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the Google homepage.;
-  // console.log(response.socket.bytesRead); // this is the bytes we get from the site.
+  // console.log(response.socket.bytesRead); // this is the uncompressed bytes we get from the site.
   // console.log(response);
   
 
@@ -27,13 +27,8 @@ request(`${args[0]}`, (error, response, body) => {
 
           fs.writeFile(`${args[1]}`, body, (err)=> {
             if (err) console.log(err);
-
-            console.log(body.length);
-
             console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`);
-
             process.exit();
-
         })} else if (answer === 'N') {
             process.exit();
         }
